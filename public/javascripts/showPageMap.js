4714ce18604d7442eaf60bd8adf38cdbebcd3bdd
Himanshi Saini <himanshi1242000@gmail.com>
2021-05-04 02:23:05 +0530

    modified maps

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,21 +1,22 @@
-const myCampground = JSON.parse(campground);
+//console.log(campground);
+//const myCampground = JSON.parse(campground);
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: myCampground.geometry.coordinates, // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9,
 });
 
 map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
-    .setLngLat(myCampground.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${myCampground.title}</h3><p>${myCampground.location}</p>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map)
